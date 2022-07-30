1192a3e5753ab63f6f6bd76c2ba1d266d1abec3c
patleon <patrihl93@gmail.com>
2022-07-30 21:21:18 +0300

    Updated demo

--- examples/demo_atomic_database/demo_clmds.py
+++ examples/demo_atomic_database/demo_clmds.py
@@ -13,7 +13,7 @@ print("________________________________________________________")
 data = clmds.clMDS(atoms='qm9_demo.xyz',
                    descriptor="quippy_soap_turbo",
                    cutoff=[2.5, 3.], do_species=['F'],
-                   sparsify='random', n_sparse=300)
+                   sparsify='random', n_sparse=500)
 
 
 print("________________________________________________________")
@@ -31,6 +31,7 @@ print("--------------------------------------------------------")
 print("--------------------------------------------------------")
 print("-----------     'Advanced' way      --------------------")
 print("--------------------------------------------------------")
+
 t0a = time.time()
 data.cluster_MDS([12,1], weight_cluster_mds=2, iter_med=100, 
                  n_jobs_cluster=8, n_jobs_anchor=8)
@@ -38,8 +39,8 @@ t1a = time.time()
 
 Y2 = data.sparse_coordinates
 C2 = data.sparse_cluster_indices
+M2 = data.sparse_medoids
 print("--------------------------------------------------------")
-
 
 print("________________________________________________________")
 print(" ")
@@ -48,13 +49,10 @@ print("________________________________________________________")
 print("--------------------------------------------------------")
 print("------------ Estimating their coord. -------------------")
 t0e = time.time()
-all_F = np.where(np.array(data.species_list) == 'F')[0]
-ind_F = np.setdiff1d(all_F, data.sparse_list)
-data.compute_atoms_estim_coordinates(indices=ind_F)
+Y_estim = data.get_estim_coordinates()
+C_estim = Y_estim[:,2].astype(int)
 t1e = time.time()
 
-Y_estim = data.estim_coordinates
-C_estim = data.estim_cluster_indices
 print("--------------------------------------------------------")
 print(" ")
 print('Time clMDS (sparse): %.2g sec'% (t1u-t0u))
@@ -65,23 +63,11 @@ print(" ")
 
 #############################################################
 ## Save file
+# in the original xyz file (only 2-dim. coord. and clustering)
 # data.write_xyz(filename='qm9_struct.xyz')
 
-clmdsfile = './clmds_qm9_demo.dat'
-f = open(clmdsfile, 'w+')
-sparse = data.sparse_list
-M2 = data.sparse_medoids
-print('n_sparse X_clmds Y_clmds C M', file=f)
-for i in range(0, len(sparse)):
-    if i in M2:
-        ind_M = 1
-    else:
-        ind_M = 0
-    print('%i %f %f %i %i' % (sparse[i], Y2[i,0], Y2[i,1],
-          C2[i], ind_M), file=f)
-f.close()
-
-
+# in a new file
+data.save_to_file()
 
 ## Plotting
 import matplotlib.pyplot as plt
@@ -92,11 +78,11 @@ color_list=np.array(['gold','limegreen','green','red','brown',
                      'rosybrown', 'coral', 'blue', 'olive'])
 plt.suptitle("cl-MDS map of F atoms (QM9 database)")
 plt.subplot(1,3,1)
-plt.title("sparse (easy)")
+plt.title("sparse (default param.)")
 plt.scatter(Y1[:, 0], Y1[:, 1], c=color_list[C1], alpha=0.4)
 
 plt.subplot(1,3,2)
-plt.title("sparse (param.)")
+plt.title("sparse (faster param.)")
 plt.scatter(Y2[:, 0], Y2[:, 1], c=color_list[C2], alpha=0.4)
 
 plt.subplot(1,3,3)
@@ -105,6 +91,7 @@ plt.scatter(Y_estim[:, 0], Y_estim[:, 1], c=color_list[C_estim],
             alpha=0.4)
 plt.scatter(Y2[:, 0], Y2[:, 1], c='black', marker='.',
             label='sparse set')
+
 plt.legend()
 
 plt.show()
